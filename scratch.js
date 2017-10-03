fd1c020ac761a9979807ce8a068be965b1306f57
Robert Dickerson <rdickeriii@gmail.com>
2017-10-03 12:44:25 -0700

    Refactor: working with knockout
    end_of_line

--- scratch.js
+++ scratch.js
@@ -1,12 +1,13 @@
 var ViewModel = function() {
-    this.clickCount = ko.observable()
-    this.name = ko.observable
-    this.imbSrc = ko.observable(img)
-    this.imgAttribution = ko.observable(HTTP)
+    this.clickCount = ko.observable();
+    this.name = ko.observable;
+    this.imbSrc = ko.observable(img);
+    this.imgAttribution = ko.observable(HTTP);
+    this.nicknames = ko.observableArray(['Zer Zer', 'Zero', 'Missa Mew Mew']);
 
     this.incrementCounter = function () {
-        this.clickCount(this.clickCount() + 1);
+        this.currentCat().clickCount(this.currentCat()clickCount() + 1);
     };
-}
+};
 
 ko.applyBindings(new ViewModel());
