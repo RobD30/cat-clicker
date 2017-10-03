a55d1d9cb5f423d90491cb7c61552739d1a742f7
Robert Dickerson <rdickeriii@gmail.com>
2017-10-03 12:22:17 -0700

    Refactor: KnockoutJS mini cat-clicker
    end_of_line

---
+++ scratch.js
@@ -0,0 +1,10 @@
+var ViewModel = function() {
+    this.clickCount = ko.observable()
+    this.name = ko.observable
+    this.imbSrc = ko.observable(img)
+    this.imgAttribution = ko.observable(HTTP)
+
+    this.incrementCounter = function () {};
+}
+
+ko.applyBindings(new ViewModel())
